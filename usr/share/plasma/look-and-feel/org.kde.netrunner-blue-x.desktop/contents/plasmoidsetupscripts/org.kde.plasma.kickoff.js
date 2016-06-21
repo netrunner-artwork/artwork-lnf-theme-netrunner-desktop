f43cfa7f50c15e987706b0d1aa50692b60bc1684
Rohan Garg <rohan@garg.io>
2016-06-21 22:06:54 +0200

    Moved to their own packages

--- usr/share/plasma/look-and-feel/org.kde.netrunner-blue-x.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++
@@ -1,8 +0,0 @@
-applet.currentConfigGroup = ["Shortcuts"]
-applet.writeConfig("global", "Alt+F1")
-
-applet.currentConfigGroup = ["General"]
-applet.writeConfig("icon", "file:///usr/share/icons/hicolor/scalable/apps/homerun.svg")
-applet.writeConfig("favorites", ["systemsettings.desktop","synaptic.desktop","org.kde.ksysguard.desktop"])
-
-applet.reloadConfig();
