0cdfd4c9d26d43b69422056080c7e6e089e3907a
Rohan Garg <rohan@garg.io>
2016-07-20 11:30:10 +0200

    Don't create a activity per screen, makes no sense, cleanup++

--- usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,15 +1,4 @@
-
 loadTemplate("org.kde.plasma.desktop.defaultPanel")
-
-// for (var i = 0; i < screenCount; ++i) {
-//     var id = createActivity("Desktop");
-//     var desktopsArray = desktopsForActivity(id);
-//     print(desktopsArray.length);
-//     for( var j = 0; j < desktopsArray.length; j++) {
-//         desktopsArray[j].wallpaperPlugin = 'org.kde.image';
-//         //var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
-//     }
-// }
 
 var actionPlugins = ConfigFile("plasma-org.kde.plasma.desktop-appletsrc");
 actionPlugins.group = "ActionPlugins";
@@ -40,24 +29,18 @@ rightButton.writeEntry("remove", "true");
 delete rightButton;
 delete actionPlugins;
 
-//loadTemplate("org.kde.plasma-desktop.defaultPanel");
-//loadTemplate("org.kde.plasma-desktop.sidebarPanel");
+var id = createActivity("Netrunner Desktop", "org.kde.plasma.folder")
+var desktopsArray = desktopsForActivity(id);
 
-for (var i = 0; i < screenCount; ++i) {
-    var id = createActivity("Netrunner Desktop", "org.kde.plasma.folder")
-    var desktopsArray = desktopsForActivity(id);
+for (var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = 'org.kde.color'
 
-    for (var j = 0; j < desktopsArray.length; j++) {
-        desktopsArray[j].wallpaperPlugin = 'org.kde.color'
-
-        desktopsArray[j].currentConfigGroup = new Array("General");
-        desktopsArray[j].writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,3")
-        desktopsArray[j].writeConfig("pressToMove",true);
-        desktopsArray[j].writeConfig("showToolbox",false);
-        desktopsArray[j].writeConfig("popups",false);
-        desktopsArray[j].writeConfig("selectionMarkers",false);
-        desktopsArray[j].writeConfig("sortMode","-1");
-        desktopsArray[j].currentConfigGroup = new Array("Wallpaper", "org.kde.color", "General");
-        desktopsArray[j].writeConfig("Color", "18,48,113");
-    }
+    desktopsArray[j].currentConfigGroup = new Array("General");
+    desktopsArray[j].writeConfig("positions","1,8,desktop:/mycomputer.desktop,0,0")
+    desktopsArray[j].writeConfig("pressToMove",true);
+    desktopsArray[j].writeConfig("popups",false);
+    desktopsArray[j].writeConfig("selectionMarkers",false);
+    desktopsArray[j].writeConfig("sortMode","-1");
+    desktopsArray[j].currentConfigGroup = new Array("Wallpaper", "org.kde.color", "General");
+    desktopsArray[j].writeConfig("Color", "18,48,113");
 }
