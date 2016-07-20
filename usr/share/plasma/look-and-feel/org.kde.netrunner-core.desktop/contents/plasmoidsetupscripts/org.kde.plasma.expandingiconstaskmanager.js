a3bdcb72f3aef0160fa001e0a4ca38fb11036c2d
Rohan Garg <rohan@garg.io>
2016-07-20 12:01:56 +0200

    Drop EITM setup, EITM is no more

--- usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/plasmoidsetupscripts/org.kde.plasma.expandingiconstaskmanager.js
+++
@@ -1,3 +0,0 @@
-applet.currentConfigGroup = ["Configuration", "General"]
-applet.writeConfig("launchers" ,"file:///usr/share/applications/org.kde.dolphin.desktop?wmClass=dolphin,file:///usr/share/applications/firefox.desktop?wmClass=Firefox")
-applet.writeConfig("showOnlyCurrentDesktop", true)
