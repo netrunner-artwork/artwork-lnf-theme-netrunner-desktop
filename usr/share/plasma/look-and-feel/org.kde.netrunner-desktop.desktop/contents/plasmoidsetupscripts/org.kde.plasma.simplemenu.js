b3276a4c0a97bf7ee68ae67c1dd457d992af83df
Rohan Garg <rohan@garg.io>
2016-09-29 15:21:39 +0200

    Update as per wiki

--- usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/plasmoidsetupscripts/org.kde.plasma.simplemenu.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/plasmoidsetupscripts/org.kde.plasma.simplemenu.js
@@ -3,9 +3,8 @@ applet.writeConfig("global", "Alt+F1")
 
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("customButtonImage", "file:///usr/share/icons/hicolor/scalable/apps/homerun.svg")
-applet.writeConfig("favoriteApps", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "org.kde.konsole.desktop", "synaptic.desktop"])
-applet.writeConfig("limitDepth", true)
-applet.writeConfig("useCustomButtonImage", true)
+applet.writeConfig("favoriteApps", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "synaptic.desktop"])
 applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop")
+applet.writeConfig("useCustomButtonImage", true)
 
 applet.reloadConfig();
