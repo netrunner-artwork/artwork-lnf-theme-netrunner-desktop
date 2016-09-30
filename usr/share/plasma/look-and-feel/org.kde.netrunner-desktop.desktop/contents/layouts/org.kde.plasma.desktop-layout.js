e2daaeb7d67339f5dbe1a11b31238b741db8e871
Rohan Garg <rohan@garg.io>
2016-09-30 11:19:09 +0200

    Fix desktop layouting, set a image as background

--- usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -33,9 +33,11 @@ delete actionPlugins;
 var desktopsArray = desktopsForActivity(currentActivity());
 
 for (var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.color'
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image'
+    desktopsArray[j].currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
+    desktopsArray[j].writeConfig("Image", "file:///usr/share/wallpapers/core-16.09/netrunner-core.png")
     desktopsArray[j].currentConfigGroup = new Array("General");
-    desktopsArray[j].writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,4,desktop:/secure-remote.desktop,0,3")
+    desktopsArray[j].writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,4")
     desktopsArray[j].writeConfig("pressToMove",true);
     desktopsArray[j].writeConfig("showToolbox",false);
     desktopsArray[j].writeConfig("popups",false);
