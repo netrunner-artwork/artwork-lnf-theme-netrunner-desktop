6aee6dbae29013db31582e02917b17087ae7d4e9
Rohan Garg <rohan@garg.io>
2016-09-30 14:48:45 +0200

    Fix script

--- usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -36,13 +36,13 @@ for (var j = 0; j < desktopsArray.length; j++) {
     desktopsArray[j].wallpaperPlugin = 'org.kde.image'
     desktopsArray[j].currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
     desktopsArray[j].writeConfig("Image", "file:///usr/share/wallpapers/core-16.09/netrunner-core.png")
+    desktopsArray[j].writeConfig("FillMode", "2")
+
     desktopsArray[j].currentConfigGroup = new Array("General");
-    desktopsArray[j].writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,4")
+    desktopsArray[j].writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,3")
     desktopsArray[j].writeConfig("pressToMove",true);
     desktopsArray[j].writeConfig("showToolbox",false);
     desktopsArray[j].writeConfig("popups",false);
     desktopsArray[j].writeConfig("selectionMarkers",false);
     desktopsArray[j].writeConfig("sortMode","-1");
-    desktopsArray[j].currentConfigGroup = new Array("Wallpaper", "org.kde.color", "General");
-    desktopsArray[j].writeConfig("Color", "18,48,113");
 }
