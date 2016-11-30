8caf0c1bd5cb5e737ec2bf33b0fd95dc0d2df121
Rohan Garg <rohan@garg.io>
2016-11-30 14:43:28 +0100

    Update for new wallpaper

--- usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -35,7 +35,7 @@ var desktopsArray = desktopsForActivity(currentActivity());
 for (var j = 0; j < desktopsArray.length; j++) {
     desktopsArray[j].wallpaperPlugin = 'org.kde.image'
     desktopsArray[j].currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
-    desktopsArray[j].writeConfig("Image", "file:///usr/share/wallpapers/netrunner-1609/netrunner-desktop-1609.png")
+    desktopsArray[j].writeConfig("Image", "file:///usr/share/wallpapers/netrunner-desktop-1612/netrunner-desktop-wallpaper-1612.png")
     desktopsArray[j].writeConfig("FillMode", "2")
 
     desktopsArray[j].currentConfigGroup = new Array("General");
