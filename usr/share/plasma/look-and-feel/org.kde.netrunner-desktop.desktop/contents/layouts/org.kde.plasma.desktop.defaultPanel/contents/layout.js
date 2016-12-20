ffce531d4df0b5bb07543ca00f7438bfc2596c7c
Rohan Garg <rohan@garg.io>
2016-12-20 13:00:21 +0100

    Don't add the volume widget to the systray but to the panel itself

--- usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/layouts/org.kde.plasma.desktop.defaultPanel/contents/layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/layouts/org.kde.plasma.desktop.defaultPanel/contents/layout.js
@@ -43,6 +43,7 @@ eitm.writeConfig("separateLaunchers", false)
 eitm.writeConfig("groupPopups", false)
 eitm.writeConfig("sortingStrategy", 1)
 
+panel.addWidget('org.kde.plasma.volume')
 var yakuakeIcon = panel.addWidget("org.kde.plasma.icon")
 yakuakeIcon.currentConfigGroup = ["General"]
 yakuakeIcon.writeConfig("applicationName", "Yakuake")
@@ -54,7 +55,7 @@ var systray = panel.addWidget("org.kde.plasma.systemtray")
 var systrayContainmentId = systray.readConfig("SystrayContainmentId")
 var systrayContainment = desktopById(systrayContainmentId)
 systrayContainment.currentConfigGroup = ["General"]
-systrayContainment.writeConfig("extraItems","org.kde.plasma.devicenotifier,org.kde.plasma.networkmanagement,org.kde.plasma.volume,org.kde.discovernotifier,org.kde.plasma.diskquota,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.printmanager")
+systrayContainment.writeConfig("extraItems","org.kde.plasma.devicenotifier,org.kde.plasma.networkmanagement,org.kde.discovernotifier,org.kde.plasma.diskquota,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.printmanager")
 systrayContainment.writeConfig("knownItems", "org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.discovernotifier,org.kde.plasma.clipboard,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.printmanager,org.kde.plasma.notifications,org.kde.kdeconnect")
 systrayContainment.writeConfig("hiddenItems","org.kde.plasma.clipboard")
 
