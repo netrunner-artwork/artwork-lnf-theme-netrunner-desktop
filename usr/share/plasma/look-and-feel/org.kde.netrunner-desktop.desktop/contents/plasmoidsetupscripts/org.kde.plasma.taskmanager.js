6911e9a35c4dbeffeecfc3f2959f00996c158547
Rohan Garg <rohan@garg.io>
2017-03-06 17:33:09 +0100

    Move everything plasmoid specific into the plasmoidsetupscripts

--- usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/plasmoidsetupscripts/org.kde.plasma.taskmanager.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-desktop.desktop/contents/plasmoidsetupscripts/org.kde.plasma.taskmanager.js
@@ -1,3 +1,8 @@
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("maxStripes", 1)
+applet.writeConfig("launchers" ,"file:///usr/share/applications/org.kde.dolphin.desktop?wmClass=Dolphin,file:///usr/share/applications/firefox.desktop")
+applet.writeConfig("showOnlyCurrentDesktop", true)
+applet.writeConfig("separateLaunchers", false)
+applet.writeConfig("groupPopups", false)
+applet.writeConfig("sortingStrategy", 1)
 applet.reloadConfig()
